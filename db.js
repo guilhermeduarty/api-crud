381c87354744814d3be54fb2bae5b4f47434efcc
guilhermeduarty <147570457+guilhermeduarty@users.noreply.github.com>
2026-03-16 19:32:08 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./database.db', (err) => {
@@ -49,56 +48,4 @@ db.run(`
     )
 `);
 
-=======
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('./database.db', (err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-    } else {
-        console.log('Conectado ao banco de dados SQLite.');
-    }
-});
-
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS clients (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-    )
-`);
-
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS sellers (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-    )
-`);
-
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        price REAL NOT NULL
-    )
-`);
-
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS movimentacoes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        client_id INTEGER,
-        seller_id INTEGER,
-        product_id INTEGER,
-        quantity INTEGER NOT NULL,
-        date TEXT NOT NULL,
-        FOREIGN KEY (client_id) REFERENCES clients (id),
-        FOREIGN KEY (seller_id) REFERENCES sellers (id),
-        FOREIGN KEY (product_id) REFERENCES products (id)
-    )
-`);
-
->>>>>>> baae0653665b1f3b393091bb8e83668623640dc1
 module.exports = db;
